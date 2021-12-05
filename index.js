c6d56cc6d3e2146ef91178d1e6bd1bb5b4dec9ae
Peter Aurigemma <pa@puter.local>
2021-12-05 18:01:51 -0500

    removed slash to test

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const server = http.createServer((req, res) => {
             res.end(content)
         });
     }
-    else if(req.url==='/about'){
+    else if(req.url==='about'){
         fs.readFile(path.join(__dirname, 'public', 'about.html'),(err, content)=>{
             if (err) throw err;
             res.writeHead(200, {'Content-Type': "text/html"});
