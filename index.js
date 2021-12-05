4d22e09cd84637e72adf1a4e3d9cc3ec04373039
Peter Aurigemma <pa@puter.local>
2021-12-05 16:33:55 -0500

    First commit for server

---
+++ index.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const fs =require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+
+    console.log(req.url);
+    if(req.url ==='/'){
+        fs.readFile(path.join(__dirname, 'public', 'index.html'),(err, content)=>{
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': "text/html"});
+            res.end(content)
+        });
+    }
+    if(req.url ==='/einstein'){
+        fs.readFile(path.join(__dirname, 'public/Einstein', 'index.html'),(err, content)=>{
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': "text/html"});
+            res.end(content)
+        });
+    }
+    if(req.url ==='/about'){
+        fs.readFile(path.join(__dirname, 'public/About', 'index.html'),(err, content)=>{
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': "text/html"});
+            res.end(content)
+        });
+    }
+    else if(req.url==='/about'){
+        fs.readFile(path.join(__dirname, 'public', 'about.html'),(err, content)=>{
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': "text/html"});
+            res.end(content)
+        });
+    }
+    else if(req.url==='/api'){
+        //display api information
+        fs.readFile(path.join(__dirname, 'public', 'pizzas.json'),(err, content)=>{
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(content)
+        });
+    }
+    else{
+        //display 404 error page
+        fs.readFile(path.join(__dirname, 'public', '404.html'),(err, content)=>{
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content)
+        });
+    }
+});
+
+const PORT =process.env.PORT || 2323
+server.listen(PORT, () => console.log('Server running...'));
+    
