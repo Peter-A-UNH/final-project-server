88472f0750c5b93fd44f02186dd0987bb5549c9f
Peter Aurigemma <pa@puter.local>
2021-12-06 22:45:56 -0500

    removed extra

--- index.js
+++ index.js
@@ -14,29 +14,14 @@ const server = http.createServer((req, res) => {
             res.end(content)
         });
     }
-    else if(req.url ==='/einstein'){
-        fs.readFile(path.join(__dirname, 'public/Einstein', 'index.html'),(err, content)=>{
-            if (err) throw err;
-            res.writeHead(200, {'Content-Type': "text/html"});
-            res.end(content)
-        });
-    }
-    else if(req.url==='/about'){
-        fs.readFile(path.join(__dirname, 'public', 'about.html'),(err, content)=>{
-            if (err) throw err;
-            res.writeHead(200, {'Content-Type': "text/html"});
-            res.end(content)
-        });
-    }
     else if(req.url==='/api'){
         //display api information
         fs.readFile(path.join(__dirname, 'public', 'pizzas.json'),(err, content)=>{
             if (err) throw err;
 
             res.setHeader("Access-Control-Allow-Origin", "*");
-            res.writeHead(200, {"Content-Type": "application/json"});
+            res.writeHead(200, {'Content-Type': 'application/json'});
             res.end(content)
-            //res.end(JSON.stringify(content))
         });
     }
     else{
