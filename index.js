8e32e5a2dd95e2fe716f83b7e966d268813b1587
Peter Aurigemma <pa@puter.local>
2021-12-06 22:36:49 -0500

    fixing else

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const server = http.createServer((req, res) => {
     console.log(req.url);
 
     if(req.url ==='/'){
-        fs.readFile(path.join(__dirname, 'public', 'index.html'),(err, content)=>{
+        fs.readFile(path.join(__dirname, 'public', 'about.html'),(err, content)=>{
             if (err) throw err;
 
             res.writeHead(200, {"Content-Type": "text/html"});
             res.end(content)
         });
     }
-    if(req.url ==='/einstein'){
+    else if(req.url ==='/einstein'){
         fs.readFile(path.join(__dirname, 'public/Einstein', 'index.html'),(err, content)=>{
             if (err) throw err;
             res.writeHead(200, {'Content-Type': "text/html"});
