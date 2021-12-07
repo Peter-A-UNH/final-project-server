b51c025fd9ea0a25ffe311cbee83b16b18af79cf
Peter Aurigemma <pa@puter.local>
2021-12-06 19:27:18 -0500

    added in /

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const server = http.createServer((req, res) => {
     if(req.url ==='/'){
         fs.readFile(path.join(__dirname, 'public', 'index.html'),(err, content)=>{
             if (err) throw err;
+            res.header("Access-Control-Allow-Origin", "*");
             res.writeHead(200, {'Content-Type': "text/html"});
             res.end(content)
         });
