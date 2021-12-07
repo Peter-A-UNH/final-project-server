adaa84555a9c0e52b30027f68b270a63a92ad031
Peter Aurigemma <pa@puter.local>
2021-12-06 22:38:29 -0500

    fixing about

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
     console.log(req.url);
 
     if(req.url ==='/'){
-        fs.readFile(path.join(__dirname, 'public', 'about.html'),(err, content)=>{
+        fs.readFile(path.join(__dirname, 'public/About', 'about.html'),(err, content)=>{
             if (err) throw err;
 
             res.writeHead(200, {"Content-Type": "text/html"});
